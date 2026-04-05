4d54cb1548dfa69bd6b1b167672fa0a0419eb0e2
Paulo <124407850+PauloSantos2002@users.noreply.github.com>
2026-04-05 19:35:59 -0300

    estudos sobre manipulação de csv e carregar arquivo na aplicação

--- pages/testes.py
+++ pages/testes.py
@@ -33,3 +33,37 @@ with col[3]:
     if st.button("Multiplicação", use_container_width=True):
         resultado = num1 * num2
         st.write(f"{resultado}") 
+        
+"-"*30
+"# Dados e graficos"
+"## Gereando e exibindo dados aleatorios"
+#-- gerando o DataFrame
+df = pd.DataFrame(
+    np.random.randn(10,3),
+    columns=["a","b","c"]
+)
+
+"### Dataframe aleatório"
+st.dataframe(df)
+
+col = st.columns(2)
+col[0].bar_chart(df)
+col[1].line_chart(df)
+
+"-"*30
+
+st.subheader("Carregar arquivo CSV")
+upload = st.file_uploader(
+    "Escolha um arquivo CSV",
+    "CSV"
+)
+
+
+if upload is not None:
+    try:
+        # -- Mostrar um pedaço do arquivo -- #
+        df_upload = pd.read_csv(upload)
+        st.success("Arquivo carregado com sucesso")
+        df_upload
+    except Exception as e:
+        st.error(f"Falha ao carregar o arquivo, erro{e}")
